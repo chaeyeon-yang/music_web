189b4b670b7e8695a688bd124b225e05bdcf6afe
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2022-02-10 22:57:33 +0900

    search artist using spotify api

---
+++ spotify-react/src/App.js
@@ -0,0 +1,96 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+    const CLIENT_ID = "db2627ef57f643d79dd2ee06be799753";
+    const REDIRECT_URI = "http://localhost:3000";
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+    const RESPONSE_TYPE = "token";
+
+    const [searchKey, setSearchKey] = useState("");
+    const [artists, setArtists] = useState([]);
+
+    const searchArtists = async (event) => {
+        event.preventDefault();
+        const { data } = await axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            params: {
+                q: searchKey,
+                type: "artist",
+            },
+        });
+
+        setArtists(data.artists.items);
+    };
+
+    const renderArtists = () => {
+        return artists.map((artist) => (
+            <div key={artist.id}>
+                {artist.images.length ? (
+                    <img width={"100%"} src={artist.images[0].url} alt="" />
+                ) : (
+                    <div>No Image</div>
+                )}
+                {artist.name}
+            </div>
+        ));
+    };
+
+    const [token, setToken] = useState("");
+
+    useEffect(() => {
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem("token");
+
+        if (!token && hash) {
+            token = hash
+                .substring(1)
+                .split("&")
+                .find((elem) => elem.startsWith("access_token"))
+                .split("=")[1];
+
+            window.location.hash = "";
+            window.localStorage.setItem("token", token);
+        }
+        setToken(token);
+    }, []);
+
+    const logout = () => {
+        setToken("");
+        window.localStorage.removeItem("token");
+    };
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Spotify React</h1>
+                {!token ? (
+                    <a
+                        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+                    >
+                        Login to Spotify
+                    </a>
+                ) : (
+                    <button onClick={logout}>Logout</button>
+                )}
+                {token ? (
+                    <form onSubmit={searchArtists}>
+                        <input
+                            type="text"
+                            onChange={(e) => setSearchKey(e.target.value)}
+                        ></input>
+                        <button type={"submit"}>Search</button>
+                    </form>
+                ) : (
+                    <h2>Please login</h2>
+                )}
+                {renderArtists()}
+            </header>
+        </div>
+    );
+}
+
+export default App;
